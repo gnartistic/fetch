8f5fba11039ab7d489dbb0a13fe429750e209503
charlie <gn4rtistic@gmail.com>
2022-09-27 21:06:05 -0500

    most up to date signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< HEAD
 function App() {
+=======
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Signup from './components/Signup/';
+import Home from './components/Home';
+// import { StoreProvider } from './utils/GlobalState';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+function App ()
+{ 
+>>>>>>> b047970 (most up to date signup page)
   return (
     <div className="App">
       <header className="App-header">
