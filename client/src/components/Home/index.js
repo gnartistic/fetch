9954c0505133e5df4feee1ab02cccec818eba9a1
Elena Carey <theelenacarey@gmail.com>
2022-10-01 14:48:53 -0500

    THings happening.

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -12,11 +12,7 @@ const Home = () =>
     return (
         <>
             <div className='container home-page' style={bg}>
-<<<<<<< HEAD
-                <Navbar/>
-=======
         <Navbar/>
->>>>>>> 8fff8941df7eefc4f3a4e234f9bb5700f101fb52
             </div>
             
             <Loader type="ball-pulse-sync" />
