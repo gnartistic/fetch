416ff2dcd5f6f5d70d7193bbc131c4d814058284
Jesse <jesseponce233@gmail.com>
2022-10-05 14:35:08 -0500

    starting with the profile page

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import Loader from 'react-loaders';
 import Navbar from '../Navbar';
-
+import { useParams } from 'react-router-dom';
+import SwipeCard from '../Swiper/swipecard';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries'
 const Profile = () =>
 {
+    const { username: userParam } = useParams(); 
+    const { loading, data } = useQuery(QUERY_USER, {
+        variables: { username: userParam}
+    });
+
+    const user = data?.user || {}; 
+    if (loading) {
+        return <div>loading...</div>;
+    }
     return (
         <>
             <div className='page'>
                 <Navbar />
                 <div className='container profile-page'>
-
+                    <h2>
+                        Viewing {user.username}'s profile'
+                    </h2>
+                </div>
+                <div className='profile-page'>
+                <SwipeCard matches={user.matches} title={`${user.username}'s Fetch Card`} />  
                 </div>
             </div>
             <Loader type="ball-pulse-sync" />
